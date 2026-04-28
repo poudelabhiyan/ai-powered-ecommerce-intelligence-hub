955bd7b797ed4250db504780ddfcc01f1610d7fc
Abhiyan Poudel <poudel.abhiyan24@gmail.com>
2026-04-28 12:59:16 -0400

    Add Python environment setup test

---
+++ etl/setup_test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import matplotlib
+import sqlalchemy
+
+print("Python environment is working correctly.")
+print("Pandas version:", pd.__version__)
+print("NumPy version:", np.__version__)
+print("Scikit-learn version:", sklearn.__version__)
+print("Matplotlib version:", matplotlib.__version__)
+print("SQLAlchemy version:", sqlalchemy.__version__)
